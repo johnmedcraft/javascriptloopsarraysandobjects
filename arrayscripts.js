a32c932e30fa26aed93d66f5d62c6588bd4597cf
johnmedcraft <jmedcraft@appriss.com>
2016-05-14 21:16:52 -0400

    Added files from Array course work

--- arrayscripts.js
+++ arrayscripts.js
@@ -1,6 +1,53 @@
-var shoppingList = ['bacon', 'coffee', 'eggs'];
-document.write(shoppingList+'<br>');
-shoppingList.push('pineapple', 'fruity pebbles');
-document.write(shoppingList+'<br>');
-shoppingList.unshift('milk', 'honey');
-document.write(shoppingList+'<br>');
+function print(html) {
+  document.write(html);
+}
+
+var playlist = [
+	['Billie Jean', 'Michael Jackson'],
+	['Smooth Criminal', 'Michael Jackson'],
+	['Little Red Corvette', 'Prince'],
+	['Purple Rain', 'Prince'],
+	['Space Oddity', 'David Bowie'],
+	['Under Pressure', 'David Bowie']
+]
+
+function printSongs( songs ) {
+  var songsHTML = '<ol>';
+  for (var i = 0; i < songs.length; i += 1) {
+    songsHTML += '<li>' + songs[i][0] + ' by ' + songs[i][1] + '</li>';
+  }
+  songsHTML += '</ol>';
+  print(songsHTML);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+printSongs(playlist);
+
+
+
+
+
+
+
+
+
+
+// ARRAY EXAMPLES
+// var shoppingList = ['bacon', 'coffee', 'eggs'];
+// shoppingList.push('pineapple', 'fruity pebbles');
+// shoppingList.unshift('milk', 'honey');
